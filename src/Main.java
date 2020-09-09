fa824b65701fabe71f0de211f25fb993378193a7
Zachary Luciano <zachary.luciano@gmail.com>
2020-09-09 16:59:32 -0600

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+/**
+ * @author zach_
+ *
+ */
+public class Main {
+	
+	public static int[] ARRAY = {20, 13 ,15, 43, 7, 27, 2, 33};
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public int[] merge_sort(int[] array) {
+		
+		int array_length = array.length;
+		int array_length_2 = array.length/2;
+		
+		
+		
+		return array;
+	}
+}
